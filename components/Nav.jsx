70a270b98a719c274e373795d395c8e71fac878f
Matthew Ewan-Laws <matt.ewanlaws@gmail.com>
2020-07-09 13:02:07 +0100

    added animations, change links

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,35 +1,84 @@
 import { useState } from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+const navContainer = {
+  start: {
+    x: -500,
+  },
+  end: {
+    x: 0,
+    transition: {
+      when: "beforeChildren",
+      stiffness: 1000,
+    },
+  },
+};
+
+const navList = {
+  start: {
+    opacity: 0,
+  },
+  end: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const child = {
+  start: {
+    left: -70,
+  },
+  end: {
+    left: 0,
+    transition: {
+      x: {
+        stiffness: 1000,
+        velocity: 100,
+      },
+    },
+  },
+};
+
+const NavLink = ({ href, children }) => (
+  <li className="link">
+    <Link href={href}>
+      <a>
+        <motion.div style={{ position: "relative" }} variants={child}>
+          {children}
+        </motion.div>
+      </a>
+    </Link>
+  </li>
+);
 
 const Nav = () => {
   const [isExpanded, setExpanded] = useState(false);
-  console.log(isExpanded);
   return (
-    <>
+    <motion.div
+      variants={navContainer}
+      initial="start"
+      animate="end"
+      className="nav-outside"
+    >
       <div className="nav-container">
         <div className={`nav ${isExpanded ? "expanded" : "hidden"}`}>
-          <ul className="nav-list">
-            <li className="link">
-              <Link href="/about">About</Link>
-            </li>
-            <li className="link">
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li className="link">
-              <Link href="/contact">Contact</Link>
-            </li>
-            <li className="link">
-              <Link href="/blog">Blog</Link>
-            </li>
-            <li className="link">
-              <Link href="/cv">CV</Link>
-            </li>
-          </ul>
+          <motion.div variants={navList} className="nav-list-container">
+            <ul className="nav-list">
+              <NavLink href="/tech">Technologies</NavLink>
+              <NavLink href="/projects">Projects</NavLink>
+              <NavLink href="/contact">Contact</NavLink>
+              <NavLink href="/about">About</NavLink>
+              <NavLink href="/cv">CV</NavLink>
+            </ul>
+          </motion.div>
         </div>
       </div>
       <div className="hamburger" onClick={() => setExpanded(!isExpanded)} />
       <div className="divider"></div>
-    </>
+    </motion.div>
   );
 };
 
