dc52e010bbcd2d5fa9c50aef6368d59f73a19f03
Matthew Ewan-Laws <matt.ewanlaws@gmail.com>
2020-07-10 14:01:49 +0100

    add page header

---
+++ components/PageHeader.jsx
@@ -0,0 +1,10 @@
+const PageHeader = () => (
+  <div className="page-header">
+    <div className="page-header-content">
+      <h2>Matthew Ewan-Laws</h2>
+    </div>
+    <div className="page-header-divider"></div>
+  </div>
+);
+
+export default PageHeader;
