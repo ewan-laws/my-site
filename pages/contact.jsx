bfab423efa5e19818ac980654a992e34905af80b
Matthew Ewan-Laws <matt.ewanlaws@gmail.com>
2020-07-28 13:25:43 +0100

    add contact info

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,11 +1,23 @@
-import Layout from "../components/Layout";
-import Heading from "../components/PageHeading";
+import LayoutSideNav from "../components/LayoutSideNav";
+
+import Project from "../components/Project";
 
 import styles from "./contact.module.scss";
 
-export default () => (
-  <div className={styles.container}>
-    <div className={styles.header}>Contact</div>
-    <div className={styles.line}>Email: matt.ewanlaws at gmail dot com</div>
-  </div>
+const Projects = () => (
+  <LayoutSideNav>
+    <div className={styles.container}>
+      <div>
+        <div className={styles.contactLine}>
+          <span className={styles.contactType}>Email:</span> matt dot ewan-laws
+          at gmail dot com
+        </div>
+        <div className={styles.contactLine}>
+          <span className={styles.contactType}>Tel:</span> +44 (0) 7807919409
+        </div>
+      </div>
+    </div>
+  </LayoutSideNav>
 );
+
+export default Projects;
