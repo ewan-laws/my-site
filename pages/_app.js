e643aeb6636f5b169d3405571cd7f81bfe44df2b
Matthew Ewan-Laws <matt.ewanlaws@gmail.com>
2020-08-01 19:57:57 +0100

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
+import { initGA, logPageView } from "../utils/analytics";
+
 import "../styles/styles.scss";
 import "../styles/page.scss";
 import "../styles/page-header.scss";
 import "../styles/content-grid.scss";
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on("routeChangeComplete", logPageView);
+  }, []);
   return <Component {...pageProps} />;
 }
